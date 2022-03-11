856addc1d731a592e0d31c0f953f1a24dc4dbdca
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-03-11 21:59:39 +0100

    horizon startup script

---
+++ horizon/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# wait for the postgres databases 
+while !</dev/tcp/horizon_database/5432; do sleep 1; done; 
+while !</dev/tcp/core_database/5432; do sleep 1; done; 
+
+# load configuration file
+rm /etc/default/stellar-horizon
+source /etc/horizon/horizon.conf
+
+
+echo "================ database initialization ================"
+stellar-horizon db init
+
+echo "================ genesis state initialization ================"
+stellar-horizon ingest init-genesis-state 
+
+echo "================ stellar horizon initialization ================"
+stellar-horizon
+    
+sleep 10000000000
